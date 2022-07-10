d6f4db6805b94c8533fc78c20b8d1be87d1c2f75
ariyo123 <89997464+ariyo123@users.noreply.github.com>
2022-07-10 19:09:53 +0100

    Update sync.bat

--- sync.bat
+++ sync.bat
@@ -3,7 +3,7 @@
 "C:\Users\malog\AppData\Local\Programs\WinSCP\WinSCP.com" ^
   /log="C:\report_movement\lol.log" /ini=nul ^
   /command ^
-    "open sftp://root:Password@IP_address/ -hostkey=""ssh-rsa 2048 20oshdsa8ucIY+VNTlqnC1mz1hZP8mAqhkTpen1Iqeg""" ^
+    "open sftp://username:Password@IP_address/ -hostkey=""ssh-rsa 2048 20oshdsa8ucIY+VNTlqnC1mz1hZP8mAqhkTpen1Iqeg""" ^
     "synchronize local C:\report_movement\source /var/bmsreports/output/" ^
     
 
